0e776a30510da61b736bf1ad186bd5ae2808fb46
Jose Fuentes <jsfuentescastillo@gmail.com>
2020-02-18 15:06:39 +0100

    Echo server to accept auth token
    Signed-off-by: Jose Fuentes <jsfuentescastillo@gmail.com>

--- cmd/echo.go
+++ cmd/echo.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/jetstack/preflight/api"
 	"github.com/spf13/cobra"
@@ -18,7 +19,7 @@ can be used to act as the server part and echo the data received by the agent.`,
 	Run: echo,
 }
 
-var echoListen string
+var echoListen, allowedToken string
 
 func init() {
 	rootCmd.AddCommand(echoCmd)
@@ -28,6 +29,13 @@ func init() {
 		"l",
 		":8080",
 		"Address where to listen.",
+	)
+	echoCmd.PersistentFlags().StringVarP(
+		&allowedToken,
+		"allowed-token",
+		"t",
+		"",
+		"If provided, will only authorize requests authenticated with the token.",
 	)
 }
 
@@ -41,6 +49,12 @@ func echo(cmd *cobra.Command, args []string) {
 }
 
 func echoHandler(w http.ResponseWriter, r *http.Request) {
+	code, err := checkAuthorization(w, r)
+	if err != nil {
+		writeError(w, err.Error(), code)
+		return
+	}
+
 	if r.Method != http.MethodPost {
 		writeError(w, fmt.Sprintf("invalid method. Expected POST, received %s", r.Method), http.StatusBadRequest)
 		return
@@ -48,7 +62,7 @@ func echoHandler(w http.ResponseWriter, r *http.Request) {
 
 	var readings []*api.DataReading
 
-	err := json.NewDecoder(r.Body).Decode(&readings)
+	err = json.NewDecoder(r.Body).Decode(&readings)
 	if err != nil {
 		writeError(w, fmt.Sprintf("decoding body: %+v", err), http.StatusBadRequest)
 		return
@@ -64,8 +78,29 @@ func echoHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 }
 
+func checkAuthorization(w http.ResponseWriter, r *http.Request) (int, error) {
+	if allowedToken != "" {
+		w.Header().Set("WWW-Authenticate", `Bearer realm="Echo"`)
+
+		s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+		if len(s) != 2 {
+			return 400, fmt.Errorf("bad request: malformed Authorization header")
+		}
+
+		if s[0] != "Bearer" {
+			return 401, fmt.Errorf("not authorized")
+		}
+
+		if s[1] != allowedToken {
+			return 401, fmt.Errorf("not authorized")
+		}
+	}
+
+	return 0, nil
+}
+
 func writeError(w http.ResponseWriter, err string, code int) {
 	fmt.Printf("-- error %d -> %s\n", code, err)
 	w.Header().Set("Content-Type", "application/json")
-	http.Error(w, fmt.Sprintf(`"{ "error": "%s", "code": %d }"`, err, code), code)
+	http.Error(w, fmt.Sprintf(`{ "error": "%s", "code": %d }`, err, code), code)
 }
