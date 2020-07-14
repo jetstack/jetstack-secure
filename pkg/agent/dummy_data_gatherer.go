4eaac0eab726b7d6a2ca6050aa62b0acbd1a4fe7
Jamie Leppard <JammyL@users.noreply.github.com>
2020-07-14 16:14:55 +0100

    Changes param1 to AlwaysFail in dummy datagatherer
    param1 is no longer needed. To keep a way of testing the
    exponential backoff this has been replaced with a boolean
    value which allows the datagathering process to always fail
    
    Signed-off-by: Jamie Leppard <JammyL@users.noreply.github.com>

--- pkg/agent/dummy_data_gatherer.go
+++ pkg/agent/dummy_data_gatherer.go
@@ -8,8 +8,8 @@ import (
 )
 
 type dummyConfig struct {
-	Param1            string `yaml:"param-1"`
-	FailedAttempts    int    `yaml:"failed-attempts"`
+	AlwaysFail        bool `yaml:"always-fail"`
+	FailedAttempts    int  `yaml:"failed-attempts"`
 	wantOnCreationErr bool
 }
 
@@ -18,13 +18,13 @@ func (c *dummyConfig) NewDataGatherer(ctx context.Context) (datagatherer.DataGat
 		return nil, fmt.Errorf("an error")
 	}
 	return &dummyDataGatherer{
-		Param1:         c.Param1,
+		AlwaysFail:     c.AlwaysFail,
 		FailedAttempts: c.FailedAttempts,
 	}, nil
 }
 
 type dummyDataGatherer struct {
-	Param1         string
+	AlwaysFail     bool
 	attemptNumber  int
 	FailedAttempts int
 }
@@ -34,8 +34,8 @@ func (c *dummyDataGatherer) Fetch() (interface{}, error) {
 	if c.attemptNumber < c.FailedAttempts {
 		err = fmt.Errorf("First %d attempts will fail", c.FailedAttempts)
 	}
-	if c.Param1 == "foo" {
-		err = fmt.Errorf("Param1 cannot be foo")
+	if c.AlwaysFail {
+		err = fmt.Errorf("This data gatherer will always fail")
 	}
 	c.attemptNumber++
 	return nil, err
