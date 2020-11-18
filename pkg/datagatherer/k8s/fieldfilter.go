7ed0f9e9d8d112acb5977d3f624daaa6a31dd01b
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-11-18 14:31:53 +0000

    Add Redact to also allow removing of fields
    Plan to use this for managedFields in all objects
    
    Signed-off-by: Charlie Egan <charlieegan3@users.noreply.github.com>

--- pkg/datagatherer/k8s/fieldfilter.go
+++ pkg/datagatherer/k8s/fieldfilter.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
+// Select removes all but the supplied fields from the resource
 func Select(fields []string, resource *unstructured.Unstructured) error {
 	// convert the object to JSON for field filtering
 	asJSON, err := json.Marshal(resource)
@@ -43,6 +44,46 @@ func Select(fields []string, resource *unstructured.Unstructured) error {
 
 	// load the filtered JSON back into the resource
 	err = json.Unmarshal(filteredObject.Bytes(), resource)
+	if err != nil {
+		return fmt.Errorf("failed to update resource: %s", err)
+	}
+
+	return nil
+}
+
+// Redact removes the supplied fields from the resource
+func Redact(fields []string, resource *unstructured.Unstructured) error {
+	// convert the object to JSON for field filtering
+	asJSON, err := json.Marshal(resource)
+	if err != nil {
+		return fmt.Errorf("failed to marshal json for resource: %s", err)
+	}
+
+	// parse the JSON for processing in gabs
+	jsonParsed, err := gabs.ParseJSON(asJSON)
+	if err != nil {
+		return fmt.Errorf("failed to parse generated json for resource: %s", err)
+	}
+
+	// craft a new object excluding redacted fields
+	for _, v := range fields {
+		// also support JSONPointers for keys containing '.' chars
+		if strings.HasPrefix(v, "/") {
+			pathComponents := strings.Split(v, "/")[1:]
+			if jsonParsed.Exists(pathComponents...) {
+				jsonParsed.Delete(pathComponents...)
+			}
+		} else {
+			if jsonParsed.ExistsP(v) {
+				jsonParsed.DeleteP(v)
+			}
+		}
+	}
+
+	fmt.Println(jsonParsed.String())
+
+	// load the filtered JSON back into the resource
+	err = json.Unmarshal(jsonParsed.Bytes(), resource)
 	if err != nil {
 		return fmt.Errorf("failed to update resource: %s", err)
 	}
--- pkg/datagatherer/k8s/fieldfilter_test.go
+++ pkg/datagatherer/k8s/fieldfilter_test.go
@@ -7,7 +7,7 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
-func TestFieldSelector(t *testing.T) {
+func TestSelect(t *testing.T) {
 	resource := &unstructured.Unstructured{
 		Object: map[string]interface{}{
 			"apiVersion": "v1",
@@ -57,7 +57,7 @@ func TestFieldSelector(t *testing.T) {
 	}
 }
 
-func TestFieldSelectorMissingSelectedField(t *testing.T) {
+func TestSelectMissingSelectedField(t *testing.T) {
 	resource := &unstructured.Unstructured{
 		Object: map[string]interface{}{
 			"kind": "Secret",
@@ -65,12 +65,84 @@ func TestFieldSelectorMissingSelectedField(t *testing.T) {
 	}
 
 	fieldsToSelect := []string{
-		"kind",
+		"kind", // required for unstructured unmarshal
 		"missing",
 		"/missing",
 	}
 
 	err := Select(fieldsToSelect, resource)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	bytes, err := json.MarshalIndent(resource, "", "    ")
+	expectedJSON := `{
+    "kind": "Secret"
+}`
+	if string(bytes) != expectedJSON {
+		t.Fatalf("unexpected JSON: \ngot \n%s\nwant\n%s", string(bytes), expectedJSON)
+	}
+}
+
+func TestRedact(t *testing.T) {
+	resource := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "v1",
+			"kind":       "Secret",
+			"metadata": map[string]interface{}{
+				"name":                       "example",
+				"namespace":                  "example",
+				"last-applied-configuration": "secret",
+			},
+			"type": "kubernetes.io/tls",
+			"data": map[string]interface{}{
+				"tls.crt": "cert data",
+				"tls.key": "secret",
+			},
+		},
+	}
+
+	fieldsToRedact := []string{
+		"metadata.last-applied-configuration",
+		"/data/tls.key",
+	}
+
+	err := Redact(fieldsToRedact, resource)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	bytes, err := json.MarshalIndent(resource, "", "    ")
+	expectedJSON := `{
+    "apiVersion": "v1",
+    "data": {
+        "tls.crt": "cert data"
+    },
+    "kind": "Secret",
+    "metadata": {
+        "name": "example",
+        "namespace": "example"
+    },
+    "type": "kubernetes.io/tls"
+}`
+	if string(bytes) != expectedJSON {
+		t.Fatalf("unexpected JSON: \ngot \n%s\nwant\n%s", string(bytes), expectedJSON)
+	}
+}
+
+func TestRedactMissingField(t *testing.T) {
+	resource := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"kind": "Secret",
+		},
+	}
+
+	fieldsToRedact := []string{
+		"missing",
+		"/missing",
+	}
+
+	err := Redact(fieldsToRedact, resource)
 	if err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	}
