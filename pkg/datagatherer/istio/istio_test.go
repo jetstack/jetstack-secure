5b359c5e6214fb99c079e8172c6ef0e5747b67b3
wwwil <wwwil.squires@gmail.com>
2021-02-05 16:50:30 +0000

    Implement full test of Istio data gatherer fetch
    Signed-off-by: wwwil <wwwil.squires@gmail.com>

--- pkg/datagatherer/istio/istio_test.go
+++ pkg/datagatherer/istio/istio_test.go
@@ -2,25 +2,190 @@ package isito
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
 	"testing"
+
+	"gopkg.in/yaml.v2"
 )
 
-// TODO: Implement proper testing.
-func TestStuff(t *testing.T) {
-	c := Config{
-		// Set this to your kubeconfig path for local testing against a cluster.
-		KubeConfigPath: "",
-	}
+const tempFilePrefix = "preflight-test-istio-datagatherer"
 
-	dg, err := c.NewDataGatherer(context.TODO())
+//// TestFetchRealCluster is not used, but has been left here for development use to allow testing against a real cluster.
+//func TestFetchRealCluster(t *testing.T) {
+//	c := Config{
+//		Resources: []schema.GroupVersionResource{
+//			{
+//				Group:    "",
+//				Version:  "v1",
+//				Resource: "namespaces",
+//			},
+//		},
+//	}
+//
+//	dg, err := c.NewDataGatherer(context.TODO())
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	results, err := dg.Fetch()
+//	if err != nil {
+//		t.Fatal(fmt.Sprintf("%T", err))
+//	}
+//
+//	t.Log(results)
+//}
+
+// TestFetch runs a full test of the Istio data gatherer; running a fake Kubernetes API, using dynamic data gatherers to
+// fetch resources, running fetched resources through Istio analysis, and checking that the analysis messaged generated
+// are what was expected.
+func TestFetch(t *testing.T) {
+	// Local server to handle requests made by Kubernetes dynamic data gatherer.
+	localServer := createLocalTestServer(t)
+
+	// Parse the URL of the server to generate the kubeconfig file.
+	parsedURL, err := url.Parse(localServer.URL)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("failed to parse test server url %s", localServer.URL)
 	}
 
-	results, err := dg.Fetch()
+	// Ensure there is a valid kubeconfig in a temporary file for the dynamic data gatherer.
+	kubeConfigPath, err := createKubeConfigWithServer(parsedURL.String())
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("failed to create temp kubeconfig: %s", err)
+	}
+	defer os.Remove(kubeConfigPath)
+
+	// Create the Config for the test.
+	config := Config{}
+	err = yaml.Unmarshal([]byte(fmt.Sprintf(configString, kubeConfigPath)), &config)
+	if err != nil {
+		t.Fatalf("unexpected error: %+v", err)
+	}
+	dg, err := config.NewDataGatherer(context.TODO())
+	if err != nil {
+		t.Fatalf("unexpected error creating data gatherer: %+v", err)
 	}
 
-	t.Log(results)
+	// Fetch analysis result from the data gatherer.
+	rawAnalysisResult, err := dg.Fetch()
+	if err != nil {
+		t.Fatalf("unexpected error fetching results: %+v", err)
+	}
+
+	// Unpack the analysis result to find the code of the first message for checking.
+	var analysisResult map[string]interface{}
+	if err := json.Unmarshal([]byte(rawAnalysisResult.(string)), &analysisResult); err != nil {
+		t.Fatalf("unexpected error unmarshalling analysis result: %+v", err)
+	}
+	analysisMessages, ok := analysisResult["Messages"].([]interface{})
+	if !ok {
+		t.Fatalf("%+v", analysisResult["Messages"])
+	}
+	analysisMessage, ok := analysisMessages[0].(map[string]interface{})
+	if !ok {
+		t.Fatalf("%+v", analysisMessages[0])
+	}
+	analysisMessageCode, ok := analysisMessage["code"].(string)
+	if !ok {
+		t.Fatalf("%+v", analysisMessage["code"])
+	}
+
+	// With the test configuration and fake resources provided there should be only one message to warn about a missing
+	// Istio annotation on the default Namespace, with code IST0102.
+	if analysisMessageCode != "IST0102" {
+		t.Fatalf("unexpected analysis result messages: %+v", analysisMessageCode)
+	}
 }
+
+var configString = `
+kubeconfig: %s
+resources:
+- group:    ""
+  version:  "v1"
+  resource: "namespaces"
+`
+
+// createKubeConfigWithServer creates a kubeconfig file on disk with a reference to the local server.
+func createKubeConfigWithServer(server string) (string, error) {
+	content := fmt.Sprintf(kuebConfigString, server)
+	tempFile, err := ioutil.TempFile("", tempFilePrefix)
+	if err != nil {
+		return "", fmt.Errorf("failed to create a tmpfile for kubeconfig")
+	}
+
+	if _, err := tempFile.Write([]byte(content)); err != nil {
+		return "", fmt.Errorf("failed to write to tmp kubeconfig file")
+	}
+	if err := tempFile.Close(); err != nil {
+		return "", fmt.Errorf("failed to close tmp kubeconfig file after writing")
+	}
+
+	return tempFile.Name(), nil
+}
+
+var kuebConfigString = `
+apiVersion: v1
+kind: Config
+clusters:
+- cluster:
+    server: %s
+  name: example
+contexts:
+- context:
+    cluster: example
+    namespace: default
+    user: test
+  name: test
+current-context: test
+users:
+- name: test
+  user:
+    username: test
+    password: test
+`
+
+// createLocalTestServer creates a local test server to respond to Kubernetes API requests from the dynamic data
+// gatherer.
+func createLocalTestServer(t *testing.T) *httptest.Server {
+	var localServer *httptest.Server
+	localServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var responseContent []byte
+
+		switch r.URL.Path {
+		case "/api/v1/namespaces":
+			responseContent = []byte(testNamespaces)
+		default:
+			t.Fatalf("Unexpected URL was called: %s", r.URL.Path)
+		}
+
+		w.Write(responseContent)
+	}))
+
+	return localServer
+}
+
+var testNamespaces = `
+{
+  "apiVersion": "v1",
+  "items": [
+    {
+      "apiVersion": "v1",
+      "kind": "Namespace",
+      "metadata": {
+        "name": "default"
+      }
+    }
+  ],
+  "kind": "List",
+  "metadata": {
+    "resourceVersion": "",
+    "selfLink": ""
+  }
+}
+`
