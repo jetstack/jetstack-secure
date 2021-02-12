686141632d8185570698e20dba9e1850fd4e87f1
wwwil <wwwil.squires@gmail.com>
2021-02-12 16:04:26 +0000

    Fix issues with fake client in test for Kubernetes dynamic data gatherer
    Signed-off-by: wwwil <wwwil.squires@gmail.com>

--- pkg/datagatherer/k8s/dynamic_test.go
+++ pkg/datagatherer/k8s/dynamic_test.go
@@ -112,7 +112,7 @@ func TestDynamicGatherer_Fetch(t *testing.T) {
 		gvr        schema.GroupVersionResource
 		namespaces []string
 		objects    []runtime.Object
-		expected   interface{}
+		expected   *unstructured.UnstructuredList
 		err        bool
 	}{
 		"an error should be returned if 'resource' is missing": {
@@ -130,101 +130,101 @@ func TestDynamicGatherer_Fetch(t *testing.T) {
 				getObject("foobar/v1", "Foo", "testfoo", "testns", false),
 			),
 		},
-		// TODO restore these test cases by adding to the new list of gvr to list type mappings, all mapp to UnstructuredList
-		//	"only Foos in the specified namespace should be returned": {
-		//		gvr:        schema.GroupVersionResource{Group: "foobar", Version: "v1", Resource: "foos"},
-		//		namespaces: []string{"testns"},
-		//		objects: []runtime.Object{
-		//			getObject("foobar/v1", "Foo", "testfoo", "testns", false),
-		//			getObject("foobar/v1", "Foo", "testfoo", "nottestns", false),
-		//		},
-		//		expected: asUnstructuredList(
-		//			getObject("foobar/v1", "Foo", "testfoo", "testns", false),
-		//		),
-		//	},
-		//	"Foos in different namespaces should be returned if no namespace field is set": {
-		//		gvr: schema.GroupVersionResource{Group: "foobar", Version: "v1", Resource: "foos"},
-		//		objects: []runtime.Object{
-		//			getObject("foobar/v1", "Foo", "testfoo", "testns1", false),
-		//			getObject("foobar/v1", "Foo", "testfoo", "testns2", false),
-		//		},
-		//		expected: asUnstructuredList(
-		//			getObject("foobar/v1", "Foo", "testfoo", "testns1", false),
-		//			getObject("foobar/v1", "Foo", "testfoo", "testns2", false),
-		//		),
-		//	},
-		//	"Secret resources should have data removed": {
-		//		gvr: schema.GroupVersionResource{Group: "", Version: "v1", Resource: "secrets"},
-		//		objects: []runtime.Object{
-		//			getSecret("testsecret", "testns1", map[string]interface{}{
-		//				"secretKey": "secretValue",
-		//			}, false, true),
-		//			getSecret("anothertestsecret", "testns2", map[string]interface{}{
-		//				"secretNumber": "12345",
-		//			}, false, true),
-		//		},
-		//		expected: asUnstructuredList(
-		//			getSecret("testsecret", "testns1", nil, false, false),
-		//			getSecret("anothertestsecret", "testns2", nil, false, false),
-		//		),
-		//	},
-		//	"Secret of type kubernetes.io/tls should have crts and not keys": {
-		//		gvr: schema.GroupVersionResource{Group: "", Version: "v1", Resource: "secrets"},
-		//		objects: []runtime.Object{
-		//			getSecret("testsecret", "testns1", map[string]interface{}{
-		//				"tls.key": "secretValue",
-		//				"tls.crt": "value",
-		//				"ca.crt":  "value",
-		//			}, true, true),
-		//			getSecret("anothertestsecret", "testns2", map[string]interface{}{
-		//				"example.key": "secretValue",
-		//				"example.crt": "value",
-		//			}, true, true),
-		//		},
-		//		expected: asUnstructuredList(
-		//			// only tls.crt and ca.cert remain
-		//			getSecret("testsecret", "testns1", map[string]interface{}{
-		//				"tls.crt": "value",
-		//				"ca.crt":  "value",
-		//			}, true, false),
-		//			// all other keys removed
-		//			getSecret("anothertestsecret", "testns2", nil, true, false),
-		//		),
-		//	},
-		//	"Foos in different namespaces should be returned if they are in the namespace list for the gatherer": {
-		//		gvr:        schema.GroupVersionResource{Group: "foobar", Version: "v1", Resource: "foos"},
-		//		namespaces: []string{"testns", "testns2"},
-		//		objects: []runtime.Object{
-		//			getObject("foobar/v1", "Foo", "testfoo", "testns", false),
-		//			getObject("foobar/v1", "Foo", "testfoo2", "testns2", false),
-		//			getObject("foobar/v1", "Foo", "testfoo3", "nottestns", false),
-		//		},
-		//		expected: asUnstructuredList(
-		//			getObject("foobar/v1", "Foo", "testfoo", "testns", false),
-		//			getObject("foobar/v1", "Foo", "testfoo2", "testns2", false),
-		//		),
-		//	},
-		//	"Resources should have managed fields removed": {
-		//		gvr: schema.GroupVersionResource{Group: "apps", Version: "v1", Resource: "deployments"},
-		//		objects: []runtime.Object{
-		//			getObject("apps/v1", "Deployment", "foo1", "testns", false),
-		//			getObject("apps/v1", "Deployment", "foo2", "testns", true),
-		//		},
-		//		expected: asUnstructuredList(
-		//			getObject("apps/v1", "Deployment", "foo1", "testns", false),
-		//			getObject("apps/v1", "Deployment", "foo2", "testns", false),
-		//		),
-		//	},
-		//	// Note that we can't test use of fieldSelector to exclude namespaces
-		//	// here as the as the fake client does not implement it.
-		//	// See go/pkg/mod/k8s.io/client-go@v0.17.0/dynamic/fake/simple.go:291
-		//	// TODO: Add a custom reactor to allow testing of fieldSelector.
+		"only Foos in the specified namespace should be returned": {
+			gvr:        schema.GroupVersionResource{Group: "foobar", Version: "v1", Resource: "foos"},
+			namespaces: []string{"testns"},
+			objects: []runtime.Object{
+				getObject("foobar/v1", "Foo", "testfoo", "testns", false),
+				getObject("foobar/v1", "Foo", "testfoo", "nottestns", false),
+			},
+			expected: asUnstructuredList(
+				getObject("foobar/v1", "Foo", "testfoo", "testns", false),
+			),
+		},
+		"Foos in different namespaces should be returned if no namespace field is set": {
+			gvr: schema.GroupVersionResource{Group: "foobar", Version: "v1", Resource: "foos"},
+			objects: []runtime.Object{
+				getObject("foobar/v1", "Foo", "testfoo", "testns1", false),
+				getObject("foobar/v1", "Foo", "testfoo", "testns2", false),
+			},
+			expected: asUnstructuredList(
+				getObject("foobar/v1", "Foo", "testfoo", "testns1", false),
+				getObject("foobar/v1", "Foo", "testfoo", "testns2", false),
+			),
+		},
+		"Secret resources should have data removed": {
+			gvr: schema.GroupVersionResource{Group: "", Version: "v1", Resource: "secrets"},
+			objects: []runtime.Object{
+				getSecret("testsecret", "testns1", map[string]interface{}{
+					"secretKey": "secretValue",
+				}, false, true),
+				getSecret("anothertestsecret", "testns2", map[string]interface{}{
+					"secretNumber": "12345",
+				}, false, true),
+			},
+			expected: asUnstructuredList(
+				getSecret("testsecret", "testns1", nil, false, false),
+				getSecret("anothertestsecret", "testns2", nil, false, false),
+			),
+		},
+		"Secret of type kubernetes.io/tls should have crts and not keys": {
+			gvr: schema.GroupVersionResource{Group: "", Version: "v1", Resource: "secrets"},
+			objects: []runtime.Object{
+				getSecret("testsecret", "testns1", map[string]interface{}{
+					"tls.key": "secretValue",
+					"tls.crt": "value",
+					"ca.crt":  "value",
+				}, true, true),
+				getSecret("anothertestsecret", "testns2", map[string]interface{}{
+					"example.key": "secretValue",
+					"example.crt": "value",
+				}, true, true),
+			},
+			expected: asUnstructuredList(
+				// only tls.crt and ca.cert remain
+				getSecret("testsecret", "testns1", map[string]interface{}{
+					"tls.crt": "value",
+					"ca.crt":  "value",
+				}, true, false),
+				// all other keys removed
+				getSecret("anothertestsecret", "testns2", nil, true, false),
+			),
+		},
+		"Foos in different namespaces should be returned if they are in the namespace list for the gatherer": {
+			gvr:        schema.GroupVersionResource{Group: "foobar", Version: "v1", Resource: "foos"},
+			namespaces: []string{"testns", "testns2"},
+			objects: []runtime.Object{
+				getObject("foobar/v1", "Foo", "testfoo", "testns", false),
+				getObject("foobar/v1", "Foo", "testfoo2", "testns2", false),
+				getObject("foobar/v1", "Foo", "testfoo3", "nottestns", false),
+			},
+			expected: asUnstructuredList(
+				getObject("foobar/v1", "Foo", "testfoo", "testns", false),
+				getObject("foobar/v1", "Foo", "testfoo2", "testns2", false),
+			),
+		},
+		"Resources should have managed fields removed": {
+			gvr: schema.GroupVersionResource{Group: "apps", Version: "v1", Resource: "deployments"},
+			objects: []runtime.Object{
+				getObject("apps/v1", "Deployment", "foo1", "testns", false),
+				getObject("apps/v1", "Deployment", "foo2", "testns", true),
+			},
+			expected: asUnstructuredList(
+				getObject("apps/v1", "Deployment", "foo1", "testns", false),
+				getObject("apps/v1", "Deployment", "foo2", "testns", false),
+			),
+		},
+		// Note that we can't test use of fieldSelector to exclude namespaces
+		// here as the as the fake client does not implement it.
+		// See go/pkg/mod/k8s.io/client-go@v0.17.0/dynamic/fake/simple.go:291
+		// TODO: Add a custom reactor to allow testing of fieldSelector.
 	}
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			gvr := schema.GroupVersionResource{Group: "foobar", Version: "v1", Resource: "foos"}
 			gvrToListKind := map[schema.GroupVersionResource]string{
-				gvr: "UnstructuredList",
+				schema.GroupVersionResource{Group: "foobar", Version: "v1", Resource: "foos"}: "UnstructuredList",
+				schema.GroupVersionResource{Group: "apps", Version: "v1", Resource: "deployments"}: "UnstructuredList",
+				schema.GroupVersionResource{Group: "", Version: "v1", Resource: "secrets"}: "UnstructuredList",
 			}
 			cl := fake.NewSimpleDynamicClientWithCustomListKinds(emptyScheme, gvrToListKind, test.objects...)
 			g := DataGathererDynamic{
@@ -249,15 +249,9 @@ func TestDynamicGatherer_Fetch(t *testing.T) {
 					t.Errorf("expected result be an UnstructuredList but wasn't")
 				}
 
-				// TODO this is pointless, we can make the expected value an unstructured.UnstructuredList and avoid this check
-				expectedList, ok := test.expected.(*unstructured.UnstructuredList)
-				if !ok {
-					t.Errorf("test expected value should be UnstructuredList but didn't")
-				}
-				// TODO remove comment, here only the list items are compared since that's all we care about
-				if diff, equal := messagediff.PrettyDiff(expectedList.Items, list.Items); !equal {
+				if diff, equal := messagediff.PrettyDiff(test.expected.Items, list.Items); !equal {
 					t.Errorf("\n%s", diff)
-					expectedJSON, _ := json.MarshalIndent(expectedList.Items, "", "  ")
+					expectedJSON, _ := json.MarshalIndent(test.expected.Items, "", "  ")
 					gotJSON, _ := json.MarshalIndent(list.Items, "", "  ")
 					t.Fatalf("unexpected JSON: \ngot \n%s\nwant\n%s", string(gotJSON), expectedJSON)
 				}
