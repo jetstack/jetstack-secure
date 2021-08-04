1ad77dc1409075b445219fdfd00a348362d4dcf2
wenlin <wenlin.yi@jetstack.io>
2021-08-04 18:33:48 +0100

    Update generate test

--- pkg/permissions/generate.go
+++ pkg/permissions/generate.go
@@ -1,7 +1,37 @@
 package permissions
 
-import "github.com/jetstack/preflight/pkg/agent"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/jetstack/preflight/pkg/agent"
+	"github.com/jetstack/preflight/pkg/datagatherer/k8s"
+)
 
 func Generate(dataGatherers []agent.DataGatherer) string {
-	return ""
+	var accumulator string = ""
+
+	for _, g := range dataGatherers {
+		if g.Kind != "k8s-dynamic" {
+			continue
+		}
+
+		genericConfig := g.Config
+		dyConfig := genericConfig.(*k8s.ConfigDynamic)
+
+		metaName := fmt.Sprint(dyConfig.GroupVersionResource.Resource)
+
+		accumulator = fmt.Sprintf(`%s
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: jetstack-secure-agent-%s-reader
+rules:
+- apiGroups: ["%s"]
+  resources: ["%s"]
+  verbs: ["get", "list", "watch"]
+---`, accumulator, metaName, dyConfig.GroupVersionResource.Group, dyConfig.GroupVersionResource.Resource)
+	}
+
+	return strings.TrimPrefix(accumulator, "\n")
 }
--- pkg/permissions/generate_test.go
+++ pkg/permissions/generate_test.go
@@ -13,7 +13,7 @@ func TestGenerate(t *testing.T) {
 		{
 			Name: "k8s/pods",
 			Kind: "k8s-dynamic",
-			Config: k8s.ConfigDynamic{
+			Config: &k8s.ConfigDynamic{
 				GroupVersionResource: schema.GroupVersionResource{
 					Version:  "v1",
 					Resource: "pods",
@@ -23,7 +23,7 @@ func TestGenerate(t *testing.T) {
 		{
 			Name: "k8s/secrets",
 			Kind: "k8s-dynamic",
-			Config: k8s.ConfigDynamic{
+			Config: &k8s.ConfigDynamic{
 				GroupVersionResource: schema.GroupVersionResource{
 					Version:  "v1",
 					Resource: "secrets",
@@ -35,7 +35,7 @@ func TestGenerate(t *testing.T) {
 	expectedOutput := `apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRole
 metadata:
-  name: jetstack-secure-agent-pods
+  name: jetstack-secure-agent-pods-reader
 rules:
 - apiGroups: [""]
   resources: ["pods"]
@@ -44,15 +44,15 @@ rules:
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRole
 metadata:
-  name: jetstack-secure-agent-secret-reader
+  name: jetstack-secure-agent-secrets-reader
 rules:
 - apiGroups: [""]
   resources: ["secrets"]
   verbs: ["get", "list", "watch"]
-`
+---`
 
 	if output := Generate(inputDatagatherers); output != expectedOutput {
-		t.Fatalf("the output %s was unexpected")
+		t.Fatalf("unexpected output \n%s \n expected: \n%s", output, expectedOutput)
 	}
 
 }
