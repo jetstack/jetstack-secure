d3a8725541b9a0661390482e1fb966213b05a50c
wenlin <wenlin.yi@jetstack.io>
2021-08-06 18:05:15 +0100

    WIP

--- pkg/permissions/generate_test.go
+++ pkg/permissions/generate_test.go
@@ -15,23 +15,14 @@ func TestGenerateRBAC(t *testing.T) {
 	// Use these test cases to check if Generate function is correct
 	testCases := []struct {
 		// expectedClusterRoles is the collection of ClusterRole
-		expectedClusterRoles []rbac.ClusterRole
-		dataGatherers        []agent.DataGatherer
-		description          string
+		expectedClusterRoleBindings []rbac.ClusterRoleBinding
+		dataGatherers               []agent.DataGatherer
+		description                 string
 	}{
 		{
 			description: "Generate RBAC struct for pods datagatherer",
 			dataGatherers: []agent.DataGatherer{
-				{
-					Name: "k8s/pods",
-					Kind: "k8s-dynamic",
-					Config: &k8s.ConfigDynamic{
-						GroupVersionResource: schema.GroupVersionResource{
-							Version:  "v1",
-							Resource: "pods",
-						},
-					},
-				},
+
 				{
 					Name: "k8s/secrets",
 					Kind: "k8s-dynamic",
@@ -42,65 +33,31 @@ func TestGenerateRBAC(t *testing.T) {
 						},
 					},
 				},
-				{
-					Name: "k8s/awspcaissuer",
-					Kind: "k8s-dynamic",
-					Config: &k8s.ConfigDynamic{
-						GroupVersionResource: schema.GroupVersionResource{
-							Group:    "awspca.cert-manager.io",
-							Version:  "v1",
-							Resource: "awspcaissuers",
-						},
-					},
-				},
 			},
-			expectedClusterRoles: []rbac.ClusterRole{
+			expectedClusterRoleBindings: []rbac.ClusterRoleBinding{
+
 				{
 					TypeMeta: metav1.TypeMeta{
-						Kind:       "ClusterRole",
+						Kind:       "ClusterRoleBinding",
 						APIVersion: "rbac.authorization.k8s.io/v1",
 					},
+
 					ObjectMeta: metav1.ObjectMeta{
-						Name: "jetstack-secure-agent-pods-reader",
+						Name: "jetstack-secure-agent-ClusterRole-reader",
 					},
-					Rules: []rbac.PolicyRule{
+
+					Subjects: []rbac.Subject{
 						{
-							Verbs:     []string{"get", "list", "watch"},
-							APIGroups: []string{""},
-							Resources: []string{"pods"},
+							Kind:      "ServiceAccount",
+							Name:      "agent",
+							Namespace: "jetstack-secure",
 						},
 					},
-				},
-				{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "ClusterRole",
-						APIVersion: "rbac.authorization.k8s.io/v1",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "jetstack-secure-agent-secrets-reader",
-					},
-					Rules: []rbac.PolicyRule{
-						{
-							Verbs:     []string{"get", "list", "watch"},
-							APIGroups: []string{""},
-							Resources: []string{"secrets"},
-						},
-					},
-				},
-				{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "ClusterRole",
-						APIVersion: "rbac.authorization.k8s.io/v1",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "jetstack-secure-agent-awspcaissuers-reader",
-					},
-					Rules: []rbac.PolicyRule{
-						{
-							Verbs:     []string{"get", "list", "watch"},
-							APIGroups: []string{"awspca.cert-manager.io"},
-							Resources: []string{"awspcaissuers"},
-						},
+
+					RoleRef: rbac.RoleRef{
+						Kind:     "ClusterRole",
+						Name:     "jetstack-secure-agent-secret-reader",
+						APIGroup: "rbac.authorization.k8s.io",
 					},
 				},
 			},
@@ -110,9 +67,10 @@ func TestGenerateRBAC(t *testing.T) {
 
 	for _, input := range testCases {
 		got := GenerateRoles(input.dataGatherers)
-		if diff, equal := messagediff.PrettyDiff(input.expectedClusterRoles, got); !equal {
+		toBeTest := GenerateBindings(got)
+		if diff, equal := messagediff.PrettyDiff(input.expectedClusterRoleBindings, toBeTest); !equal {
 			t.Errorf("%s:\n%s", input.description, diff)
-			t.Fatalf("unexpected difference in RBAC cluster role: \ngot \n%v\nwant\n%v", got, input.expectedClusterRoles)
+			t.Fatalf("unexpected difference in RBAC cluster role: \ngot \n%v\nwant\n%v", got, input.expectedClusterRoleBindings)
 		}
 	}
 }
