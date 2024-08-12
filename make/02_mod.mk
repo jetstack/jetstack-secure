f2b8f7dc0b7c3d96e28b6dc012b9385a36b1c499
Maël Valais <mael@vls.dev>
2024-08-12 19:38:57 +0200

    makefile-modules: use the "tools" module to use yq

--- make/02_mod.mk
+++ make/02_mod.mk
@@ -141,3 +141,17 @@ ci-build: ci-test build build-docker-image build-all-platforms bundle-all-platfo
 
 ci-publish: ci-build push-docker-image
 	echo "ci-publish is going to be disabled. We are adopting Github actions"
+
+## Targets already migrated to makefile-modules.
+
+helm_chart_source_dir := deploy/charts/venafi-kubernetes-agent
+
+.PHONY: generate-manifests
+generate-manifests: ## Generates jetstack.io_venaficonnections.yaml.
+generate-manifests: | $(NEEDS_GO) $(NEEDS_YQ)
+	$(GO) run ./make/connection_crd > $(helm_chart_source_dir)/crd_bases/jetstack.io_venaficonnections.yaml
+	@echo "# DO NOT EDIT. Use 'make generate-manifests' to regenerate." >$(helm_chart_source_dir)/templates/venafi-connection-crd.without-validations.yaml
+	$(YQ) 'del(.. | ."x-kubernetes-validations"?) | del(.metadata.creationTimestamp)' $(helm_chart_source_dir)/crd_bases/jetstack.io_venaficonnections.yaml >>$(helm_chart_source_dir)/templates/venafi-connection-crd.without-validations.yaml
+
+	@echo "# DO NOT EDIT. Use 'make generate-manifests' to regenerate." >$(helm_chart_source_dir)/templates/venafi-connection-crd.yaml
+	$(YQ) 'del(.metadata.creationTimestamp)' $(helm_chart_source_dir)/crd_bases/jetstack.io_venaficonnections.yaml >> $(helm_chart_source_dir)/templates/venafi-connection-crd.yaml
