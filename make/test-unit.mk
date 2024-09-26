d2e2b6a9f18e89c9ccfb202072bb8dfa14cda868
Maël Valais <mael@vls.dev>
2024-09-26 16:39:38 +0200

    make: date -uR produced a comma that was misinterpreted in go_$*_ldflags
    Using set -x, I noticed that LDFLAGS= was being split on the commas:
    
     + LDFLAGS='-X version.Commit=6b3b365dbf4a3a907b6fd97d745efbd889afde0f -X version.BuildDate=Thu,'
     + 26 Sep 2024 14:34:51 '+0000 -X client.ClientID=k3TrDbfLhCgnpAbOiiT2kIE1AbovKzjo
    
    To fix this, I've decided to print the date without spaces or special
    chars.
    
    Before, it looked like this:
      Fri, 27 Sep 2024 10:15:10 +0000
    Now, it looks like this:
      2024-09-27-12:15:16-CEST

--- make/test-unit.mk
+++ make/test-unit.mk
@@ -1,7 +1,7 @@
 .PHONY: test-unit
 ## Unit tests
 ## @category Testing
-test-unit: | $(NEEDS_GO) $(NEEDS_GOTESTSUM) $(ARTIFACTS)
+test-unit: | $(NEEDS_GO) $(NEEDS_GOTESTSUM) $(ARTIFACTS) $(NEEDS_ETCD) $(NEEDS_KUBE-APISERVER)
 	$(GOTESTSUM) \
 		--junitfile=$(ARTIFACTS)/junit-go-e2e.xml \
 		-- \
@@ -10,4 +10,5 @@ test-unit: | $(NEEDS_GO) $(NEEDS_GOTESTSUM) $(ARTIFACTS)
 		-- \
 		-ldflags $(go_preflight_ldflags)
 
+    export KUBEBUILDER_ASSETS=$(CURDIR)/$(bin_dir)/tools
 	$(GO) tool cover -html=$(ARTIFACTS)/filtered.cov -o=$(ARTIFACTS)/filtered.html
