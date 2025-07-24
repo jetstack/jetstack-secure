398cfd49f6f8e8049d0a65aeb2459919aec87317
Richard Wall <richard.wall@cyberark.com>
2025-07-24 11:50:49 +0100

    make fix-golangci-lint
    Signed-off-by: Richard Wall <richard.wall@cyberark.com>

--- pkg/clusteruid/clusteruid_test.go
+++ pkg/clusteruid/clusteruid_test.go
@@ -3,11 +3,10 @@ package clusteruid
 import (
 	"testing"
 
-	"k8s.io/client-go/kubernetes/fake"
-
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
 func TestGetClusterUID(t *testing.T) {
