d54d435b7d4994ec0800adda04c1bdcd845de4bf
Richard Wall <richard.wall@venafi.com>
2025-08-09 12:25:44 +0100

    Delete unused client_cyberark file to avoid import cycles
    Signed-off-by: Richard Wall <richard.wall@venafi.com>

--- pkg/client/client_cyberark.go
+++
@@ -1,9 +0,0 @@
-package client
-
-import (
-	"github.com/jetstack/preflight/pkg/internal/cyberark/dataupload"
-)
-
-type CyberArkClient = dataupload.CyberArkClient
-
-var NewCyberArkClient = dataupload.NewCyberArkClient
