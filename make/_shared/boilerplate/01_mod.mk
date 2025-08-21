095d00153bba8a23e57620832c3d69d815656c85
Tim Ramlot <42113979+inteon@users.noreply.github.com>
2025-08-21 05:24:28 +0200

    upgrade makefile module to the latest version
    Signed-off-by: Tim Ramlot <42113979+inteon@users.noreply.github.com>

--- make/_shared/boilerplate/01_mod.mk
+++ make/_shared/boilerplate/01_mod.mk
@@ -12,34 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifndef repo_name
-$(error repo_name is not set)
-endif
-
 base_dir := $(dir $(lastword $(MAKEFILE_LIST)))/base/
-base_dependabot_dir := $(dir $(lastword $(MAKEFILE_LIST)))/base-dependabot/
 
-ifdef repository_base_no_dependabot
-.PHONY: generate-base
-## Generate base files in the repository
+.PHONY: verify-boilerplate
+## Verify that all files have the correct boilerplate.
 ## @category [shared] Generate/ Verify
-generate-base:
-	cp -r $(base_dir)/. ./
-	cd $(base_dir) && \
-		find . -type f | while read file; do \
-			sed "s|{{REPLACE:GH-REPOSITORY}}|$(repo_name:github.com/%=%)|g" "$$file" > "$(CURDIR)/$$file"; \
-		done
-else
-.PHONY: generate-base
-## Generate base files in the repository
+verify-boilerplate: | $(NEEDS_BOILERSUITE)
+	$(BOILERSUITE) .
+
+shared_verify_targets += verify-boilerplate
+
+.PHONY: generate-license
+## Generate LICENSE file in the repository
 ## @category [shared] Generate/ Verify
-generate-base:
+generate-license:
 	cp -r $(base_dir)/. ./
-	cd $(base_dir) && \
-		find . -type f | while read file; do \
-			sed "s|{{REPLACE:GH-REPOSITORY}}|$(repo_name:github.com/%=%)|g" "$$file" > "$(CURDIR)/$$file"; \
-		done
-	cp -r $(base_dependabot_dir)/. ./
-endif
 
 shared_generate_targets += generate-base
