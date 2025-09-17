feb7506fa91a3b79e579237019b1694fae49f3dc
Mladen Rusev <mladen.rusev@venafi.com>
2025-09-17 13:39:16 +0300

    add venctl and step as additional tools

--- make/extra_tools.mk
+++ make/extra_tools.mk
@@ -4,3 +4,6 @@ ADDITIONAL_GO_DEPENDENCIES :=
 # https://pkg.go.dev/github.com/helm-unittest/helm-unittest?tab=versions
 ADDITIONAL_TOOLS += helm-unittest=v0.8.2
 ADDITIONAL_GO_DEPENDENCIES += helm-unittest=github.com/helm-unittest/helm-unittest/cmd/helm-unittest
+
+ADDITIONAL_TOOLS += venctl=1.16.0
+ADDITIONAL_TOOLS += step=0.28.2
